2f35d9e163f317fd69b4a58dd4db6c17997f0919
C-o-m-o-n <comon928@gmail.com>
2024-01-03 07:58:14 +0300

    pull

--- main.py
+++ main.py
@@ -1,28 +1,29 @@
 # Word Puzzle solver
-
 import random
 
+#get the letters from the user
 inp = input("Enter the letters -->: ")
 
+#convert the string of the letters into a list
 letters = list(inp)
 
-wordListPath = "/home/comon/Dev/lesp/wordlist.txt"
-
-with open(wordListPath, 'r') as f:
-    wordList = f.read()
-
+#wordListPath = "/home/comon/Dev/lesp/wordlist.txt"
+#with open(wordListPath, 'r') as f:
+#    wordList = f.read()
 # print(wordList)
 
-found = False
-while not found:
-    random.shuffle(letters)
-    word = ''.join(letters)
-    print(word)
+def shuffleWord():
+    found = False #set found to false
+    while not found:
+        random.shuffle(letters)
+        word = ''.join(letters)
+        print(word)
 
-    check = input("is that the word? \n[Y/N] -->: ")
+        check = input("is that the word? \n[Y/N] -->: ")
 
-    if check == "y":
-        found = True
-    elif check == "n":
-        continue
+        if check == "y":
+            found = True
+        elif check == "n":
+            continue
 
+shuffleWord()
